6887440198ffabf904c1d36f826a44ce27babe13
atikalg <who.atik@gmail.com>
2022-04-26 17:56:43 +0530

    Added : Validation of last name

--- UserReg.java
+++ UserReg.java
@@ -1,24 +1,38 @@
 package com.bl.regex;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserReg {
-
+	Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("Enter a user name");
-		Scanner sc = new Scanner(System.in);
-		System.out.println(firstName(sc.nextLine()));
-		boolean result = firstName("Atik");
-		if(result) {
-			System.out.println("User name is valid");
+		UserReg userReg = new UserReg();
+		//function call
+		userReg.checkFirstName();
+		userReg.checkLastName();
+	}
+	//validating first name  of user
+	public void checkFirstName() {
+		System.out.println("Enter first name start with Capital letter :");
+		String firstName = sc.nextLine();
+		if( Pattern.matches("^[A-Z]{1}[a-z]{2,}",firstName) ){
+			System.out.println("first name is valid");
 		}
 		else {
-			System.out.println("User name is invalid");
+			System.out.println("first name invalid");
 		}
 	}
-	
-	public static boolean firstName(String firstName) {
-		return firstName.matches("^[A-Z]{1}[a-z]{2,}$");
+	//validating last name 
+	public void checkLastName() {
+		System.out.println("Enter Last name start with Capital letter : ");
+		String lastName = sc.nextLine();
+		if(Pattern.matches("^[A-Z]{1}[a-z]{2,}",lastName)) {
+			System.out.println("Last name is valid");
+		}
+		else {
+			System.out.println("Last name invalid");
+		}
 	}
 
 }
